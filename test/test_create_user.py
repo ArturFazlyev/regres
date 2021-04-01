f7d8704efebe1466b52548f61236c6b7cdb93032
Артур <fazlyev.art@gmail.com>
2021-04-01 22:56:49 +0500

    test

--- test/test_create_user.py
+++ test/test_create_user.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 import jsonpickle
+from assertpy import assert_that
 
 from api.user import CreateUser
 from config.config import *
@@ -12,17 +13,17 @@ create = CreateUser(URL)
 def test_create_user():
     data = jsonpickle.encode(User(name=NAME, job=JOB))
     response = create.create_user_request(data=data)
-    assert response.status_code == HTTPStatus.CREATED
-    assert isinstance(response.json()["id"], str)
+    assert_that(response.status_code).is_equal_to(HTTPStatus.CREATED)
+    assert_that(isinstance(response.json()["id"], str)).is_equal_to(True)
 
 
 def test_update_user():
     data = jsonpickle.encode(User(name="morp", job="QA Automation"))
     response = create.update_user(data=data)
-    assert response.status_code == HTTPStatus.OK
-    assert isinstance(response.json()["updatedAt"], str)
+    assert_that(response.status_code).is_equal_to(HTTPStatus.OK)
+    assert_that(isinstance(response.json()["updatedAt"], str)).is_equal_to(True)
 
 
 def test_get_user():
     response = create.get_single_user()
-    assert response.status_code == HTTPStatus.OK
+    assert_that(response.status_code).is_equal_to(HTTPStatus.OK)
